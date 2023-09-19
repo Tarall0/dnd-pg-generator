a69729b0b58e1939f0a7210809e7bacdb53393d1
Tarallo <tarantinofranc3sco@gmail.com>
2023-09-19 13:15:41 +0200

    .

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let clicks = 0;
 const button = document.getElementById("clicker");
 
 const generatePg = () => {
-
     const classespg = ["Warlock", "Druid", "Warrior", "Paladin", "Unicorn"];
     const races = ["Human", "Elf", "Half-Elf", "Dwarf", "Half-Orc", "Halfling", "Gnome"];
+    let life;
 
     const classpg = classespg[Math.floor(Math.random() * classespg.length)];
     const racepg = races[Math.floor(Math.random() * races.length)];
@@ -12,8 +12,8 @@ const generatePg = () => {
     document.getElementById("classPg").textContent = classpg;
     document.getElementById("race").textContent = racepg;
 
-    //Set stats based on race of the pg as per 3.5
-    switch(racepg){
+    // Set stats based on race of the pg as per 3.5
+    switch (racepg) {
         case "Human":
             document.getElementById("modifier").textContent = "Nessuno";
             break;
@@ -24,7 +24,7 @@ const generatePg = () => {
             document.getElementById("modifier").textContent = "+2 Destrezza, -2 Costituzione";
             break;
         case "Gnome":
-            document.getElementById("modifier").textContent = "+2 Costituzione, -2 Costituzione";
+            document.getElementById("modifier").textContent = "+2 Costituzione, -2 Carisma";
             break;
         case "Half-Elf":
             document.getElementById("modifier").textContent = "Nessuno";
@@ -39,8 +39,16 @@ const generatePg = () => {
             break;
     }
 
-
-}
+    // Set stats based on class of the pg as per 3.5
+    switch (classpg) {
+        case "Warlock":
+            life = Math.floor(Math.random() * 4) + 1;
+            document.getElementById("life").textContent = life;
+            break;
+        default:
+            break;
+    }
+};
 
 const incrementClickCount = () => {
     clicks++;
