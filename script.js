428aea577b43c9042243f5f2468389099afb2c48
Tarallo <tarantinofranc3sco@gmail.com>
2023-09-20 10:48:47 +0200

    objects

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
-let clicks = 0;
-const button = document.getElementById("clicker");
+// Define an object to store race information
+const raceInfo = {
+    "Human": { modifier: "Nessuno" },
+    "Dwarf": { modifier: "+2 Costituzione, -2 Carisma" },
+    "Elf": { modifier: "+2 Destrezza, -2 Costituzione" },
+    "Gnome": { modifier: "+2 Costituzione, -2 Carisma" },
+    "Half-Elf": { modifier: "Nessuno" },
+    "Half-Orc": { modifier: "+2 Forza, -2 Intelligenza" },
+    "Halfling": { modifier: "+2 Destrezza, -2 Forza" },
+};
 
-const generatePg = () => {
-    const classespg = ["Warlock", "Druid", "Warrior", "Paladin", "Unicorn"];
-    const races = ["Human", "Elf", "Half-Elf", "Dwarf", "Half-Orc", "Halfling", "Gnome"];
-    let life;
+// Define an object to store class information
+const classInfo = {
+    "Warlock": { lifeRange: [1, 4] },
+    "Mage": { lifeRange: [1, 4] },
+    "Bard": { lifeRange: [1, 6] },
+    "Thief": { lifeRange: [1, 6] },
+    "Druid": { lifeRange: [1, 8] },
+    "Warrior": { lifeRange: [1, 10] },
+    "Paladin": { lifeRange: [1, 10] },
+    "Barbarian": { lifeRange: [1, 12] },
+};
 
+// Function to calculate random life points based on class information
+function calculateLifePoints(classInfo) {
+    const [min, max] = classInfo.lifeRange;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to generate a character sheet
+function generatePg() {
+    // Get random class and race
+    const classespg = Object.keys(classInfo);
+    const races = Object.keys(raceInfo);
     const classpg = classespg[Math.floor(Math.random() * classespg.length)];
     const racepg = races[Math.floor(Math.random() * races.length)];
 
+    // Set the class and race in the HTML
     document.getElementById("classPg").textContent = classpg;
     document.getElementById("race").textContent = racepg;
 
-    // Set stats based on race of the pg as per 3.5
-    switch (racepg) {
-        case "Human":
-            document.getElementById("modifier").textContent = "Nessuno";
-            break;
-        case "Dwarf":
-            document.getElementById("modifier").textContent = "+2 Costituzione, -2 Carisma";
-            break;
-        case "Elf":
-            document.getElementById("modifier").textContent = "+2 Destrezza, -2 Costituzione";
-            break;
-        case "Gnome":
-            document.getElementById("modifier").textContent = "+2 Costituzione, -2 Carisma";
-            break;
-        case "Half-Elf":
-            document.getElementById("modifier").textContent = "Nessuno";
-            break;
-        case "Half-Orc":
-            document.getElementById("modifier").textContent = "+2 Forza, -2 Intelligenza";
-            break;
-        case "Halfling":
-            document.getElementById("modifier").textContent = "+2 Destrezza, -2 Forza";
-            break;
-        default:
-            break;
-    }
+    // Set the race modifier based on race
+    document.getElementById("modifier").textContent = raceInfo[racepg].modifier;
 
-    // Set stats based on class of the pg as per 3.5
-    switch (classpg) {
-        case "Warlock":
-            life = Math.floor(Math.random() * 4) + 1;
-            document.getElementById("life").textContent = life;
-            break;
-        default:
-            break;
-    }
-};
+    // Set the life points based on class
+    document.getElementById("life").textContent = calculateLifePoints(classInfo[classpg]);
+}
 
-const incrementClickCount = () => {
+// Function to increment the click count
+function incrementClickCount() {
     clicks++;
     document.getElementById("clickCount").textContent = clicks;
-};
+}
 
-button.addEventListener("click", () => {
+// Initialize click count
+let clicks = 0;
+
+
+// Get the button element
+const button = document.getElementById("clicker");
+
+// Add a click event listener to the button
+ button.addEventListener("click", () => {
+    // Generate character sheet and increment click count
     generatePg();
     incrementClickCount();
 });
+
