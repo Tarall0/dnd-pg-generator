dbf53698851d6f264f2ab776e59a3ad912332cfd
Tarallo <tarantinofranc3sco@gmail.com>
2023-09-22 21:18:38 +0200

    .

--- script/dragdrop.js
+++ script/dragdrop.js
@@ -6,9 +6,14 @@ function allowDrop(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
   }
   
-  function drop(ev) {
+  function drop(ev, targetId) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+    var targetElement = document.getElementById(targetId);
+    var draggedElement = document.getElementById(data);
 
+    // Swap the positions of the dragged element and the target element
+    var temp = targetElement.innerHTML;
+    targetElement.innerHTML = draggedElement.innerHTML;
+    draggedElement.innerHTML = temp;
+  }
