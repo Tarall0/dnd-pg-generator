f55158659324ec198b6ecc53eca895a00b5f6d26
Tarallo <tarantinofranc3sco@gmail.com>
2023-09-22 20:36:13 +0200

    drag n drop

--- script/script.js
+++ script/script.js
@@ -818,3 +818,17 @@ button.addEventListener("click", () => {
     incrementClickCount();
 });
 
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
